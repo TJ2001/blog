7ee3a349a998733fe3355c00066935ff64d7b91c
TJ2001 <jung.m.tim@gmail.com>
2016-11-11 02:56:56 -0800

    add two new triggers selections to scripts.js

--- Main/js/slider.js
+++ Main/js/slider.js
@@ -1,11 +1,31 @@
 $( document ).ready(function() {
-  $('.numberMenu').click(function(){
-    if($('.card').hasClass('clicked')){
-      $('.card').removeClass('clicked');
+  $('.trigger1').click(function(){
+    if($('.card1').hasClass('clicked')){
+      $('.card1').removeClass('clicked');
       // $('.card').addClass('maximized');
     }else{
     // $(this).removeClass('maximized');
-    $('.card').addClass('clicked');
+    $('.card1').addClass('clicked');
+    }
+  });
+
+  $('.trigger2').click(function(){
+    if($('.card2').hasClass('clicked')){
+      $('.card2').removeClass('clicked');
+      // $('.card').addClass('maximized');
+    }else{
+    // $(this).removeClass('maximized');
+    $('.card2').addClass('clicked');
+    }
+  });
+
+  $('.trigger3').click(function(){
+    if($('.card3').hasClass('clicked')){
+      $('.card1').removeClass('clicked');
+      // $('.card').addClass('maximized');
+    }else{
+    // $(this).removeClass('maximized');
+    $('.card3').addClass('clicked');
     }
   });
 });
