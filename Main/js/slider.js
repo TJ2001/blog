d174f2f10a079d52e1d427c436c37c7e2003fe44
TJ2001 <jung.m.tim@gmail.com>
2016-11-15 21:12:10 -0800

    add remove animate on card1 succesfully

--- Main/js/slider.js
+++ Main/js/slider.js
@@ -1,41 +1,41 @@
 $( document ).ready(function() {
+  $('#card1').addClass('fadeInUp');
+  $('#card1').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+    $('#card1').removeClass('fadeInUp');
+  });
   $('.trigger1').click(function(){
-    if($('.card1').hasClass('clicked')){
-      $('.card1').removeClass('clicked');
+    if($('.card1').hasClass('fly') || $('.card2').hasClass('appear')) {
+      $('.card1').removeClass('fly');
       $('.card1').removeClass('disappear');
+      $('.card2').addClass('fly');
+      $('.card2').removeClass('appear');
     }else{
-      $('.card1').addClass('clicked');
-      setTimeout(function(){
-        $('.card1').addClass('disappear');
-      }, 500);
+      // setTimeout(function(){
+      //   $('.card2').removeClass('appear');
+      //   $('.card1').addClass('appear');
+      // }, 500);
     }
   });
-
-
-  // $('.trigger1').click(function(){
-  //   if($('.card1').hasClass('clicked')){
-  //     $('.card1').removeClass('clicked');
-  //   }else{
-  //   $('.card1').addClass('clicked');
-  //   }
-  // });
 
   $('.trigger2').click(function(){
-    if($('.card2').hasClass('clicked')){
-      $('.card2').removeClass('clicked');
-    }else{
-    $('.card2').addClass('clicked');
-    }
+    // if($('.card2').hasClass('appear')){
+    //   $('.card2').removeClass('fly');
+    //   $('.card2').addClass('appear');
+    // }else{
+      $('.card1').addClass('fly');
+      setTimeout(function(){
+        $('.card1').addClass('disappear');
+        $('.card2').addClass('appear');
+      }, 500);
+
+    // }
   });
 
-  $('.trigger3').click(function(){
-    if($('.card3').hasClass('clicked')){
-      $('.card3').removeClass('clicked');
-    }else{
-    $('.card3').addClass('clicked');
-    }
-  });
+  // $('.trigger3').click(function(){
+  //   if($('.card3').hasClass('clicked')){
+  //     $('.card3').removeClass('clicked');
+  //   }else{
+  //   $('.card3').addClass('clicked');
+  //   }
+  // });
 });
-
-
-// move it up. then disappear. then appear card two.
