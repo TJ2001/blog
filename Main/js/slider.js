38ee8a118daaa0ed925fbfc09264dbc7ec67428e
TJ2001 <jung.m.tim@gmail.com>
2016-11-15 23:55:07 -0800

    make card1 button functional, card2 button is erroring by making card1 appear sometimes

--- Main/js/slider.js
+++ Main/js/slider.js
@@ -4,16 +4,25 @@ $( document ).ready(function() {
     $('#card1').removeClass('fadeInUp');
   });
   $('.trigger1').click(function(){
-    if($('.card1').hasClass('fly') || $('.card2').hasClass('appear')) {
-      $('.card1').removeClass('fly');
-      $('.card1').removeClass('disappear');
+    if($('.card2').hasClass('appear')) {
       $('.card2').addClass('fly');
-      $('.card2').removeClass('appear');
-    }else{
-      // setTimeout(function(){
-      //   $('.card2').removeClass('appear');
-      //   $('.card1').addClass('appear');
-      // }, 500);
+      setTimeout(function(){
+        $('.card2').removeClass('appear');
+      }, 500);
+      $('.card1').addClass('fadeInUp');
+      $('#card1').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
+        $('#card1').removeClass('fadeInUp');
+      });
+      // $('.card1').removeClass('fly');
+      // $('.card1').removeClass('disappear');
+      // $('.card2').addClass('fly');
+      // $('.card2').removeClass('appear');
+    // }else if($('.card2').hasClass('appear')){
+    //   $('.card1').addClass('fly');
+    //   setTimeout(function(){
+    //     $('.card2').removeClass('appear');
+    //     $('.card1').addClass('appear');
+    //    }, 500);
     }
   });
 
@@ -23,13 +32,15 @@ $( document ).ready(function() {
     //   $('.card2').addClass('appear');
     // }else{
       $('.card1').addClass('fly');
-      // setTimeout(function(){
-      //   $('.card1').addClass('disappear');
-      //   $('.card2').addClass('appear');
-      // }, 500);
-      $('#card2').addClass('animated fadeInUp appear');
+      $('.card2').addClass('appear');
+      setTimeout(function(){
+      $('.card1').addClass('disappear');
+      $('.card1').removeClass('fly');
+    }, 500);
+      $('#card2').addClass('animated fadeInUp');
       $('#card2').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function(){
         $('#card2').removeClass('fadeInUp');
+        $('#card1').removeClass('disappear');
     })
   });
 
